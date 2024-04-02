b3598f9e3662760bfede97ca767f25003e7c5f8f
Jordy Schreuders <3071062+99linesofcode@users.noreply.github.com>
2024-04-03 00:07:30 +0200

    feat(display-toggle): simple display toggle shell script
    Needed something quick and dirty. It's not perfect yet as extending
    doesn't correctly reload wallpaper images and I believe I saw waybar
    crash once or twice.
    
    People use Kanshi to define output profiles when hotplugging monitors.
    That's also what swww seems to recommend so, to be continued..

---
+++ .config/hypr/scripts/display-toggle.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+MONITOR_STATE=$(hyprctl monitors all | awk '/Monitor/ { print $2 }')
+
+set -- $MONITOR_STATE
+
+if [ $# -eq "1" ]; then
+	exit
+fi
+
+STATE=$(awk -F '=' '/DISPLAY_STATE=/ {print $2}' ~/dotfiles/.config/hypr/scripts/settings.conf)
+FILEPATH="$HOME/dotfiles/.config/hypr/scripts/settings.conf"
+
+case $STATE in
+Primary)
+	sed -i 's/DISPLAY_STATE=Primary/DISPLAY_STATE=Secondary/' $FILEPATH
+	hyprctl keyword monitor "$1,highres,0x0,2"
+	hyprctl keyword monitor "$2,disable"
+	notify-send -t 3000 -i video-display "Laptop screen only"
+	;;
+Secondary)
+	sed -i 's/DISPLAY_STATE=Secondary/DISPLAY_STATE=Extend/' $FILEPATH
+	hyprctl keyword monitor "$2,preferred,auto,2"
+	hyprctl keyword monitor "$1,disable"
+	notify-send -t 3000 -i video-display "Second screen only"
+	;;
+Extend)
+	sed -i 's/DISPLAY_STATE=Extend/DISPLAY_STATE=Primary/' $FILEPATH
+	hyprctl keyword monitor "$1,highres,0x0,2"
+	notify-send -t 3000 -i video-display "Extend screens"
+	;;
+esac
